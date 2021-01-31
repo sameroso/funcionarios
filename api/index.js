68517d5fbec089db8dfc2dee7d2a2895f848e1c6
Samer <samer.kayali@nickbuilder.com.br>
2021-01-30 23:22:47 -0300

    mudança na estrutura do projeto

--- api/index.js
+++ api/index.js
@@ -18,5 +18,14 @@ app.use(bodyParser.json());
 employeeRoutes(app)
 
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+  
+	const path = require('path');
+	app.get('*', (req, res) => {
+	  res.sendFile(path.resolve(__dirname,'seidor', 'build', 'index.html'));
+	});
+  }
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
