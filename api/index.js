450750ef4dc5a87cf54afdfbb6005d0e2482202b
Samer <samer.kayali@nickbuilder.com.br>
2021-01-30 23:41:53 -0300

    adding cors

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const employeeRoutes = require('./routes/employeeRoute')
+const cors = require("cors");
+const employeeRoutes = require("./routes/employeeRoute");
 require("./models/personModel");
 require("./models/userModel");
-
 
 mongoose.connect(
   "mongodb+srv://samer:AbZpTQr6MSrUf11w@cluster0.on79n.mongodb.net/people?retryWrites=true&w=majority",
@@ -13,19 +13,19 @@ mongoose.connect(
 
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 
-employeeRoutes(app)
+employeeRoutes(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-  
-	const path = require('path');
-	app.get('*', (req, res) => {
-	  res.sendFile(path.resolve(__dirname,'seidor', 'build', 'index.html'));
-	});
-  }
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "seidor", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
